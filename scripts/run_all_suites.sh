9cc6b341c6d188655c3009d233fea6fc029f2ee1
Julian <julian.deller-yee@tum.de>
2026-06-30 13:42:14 +0200

    update run_all_suites.sh

--- scripts/run_all_suites.sh
+++ scripts/run_all_suites.sh
@@ -8,34 +8,34 @@ export ARGON_THREADS="${ARGON_THREADS:-1 2 3 4 6 8 11 16 23 32 37 45 50 56}"
 # APRIL
 # -------------------------
 
-GB_REPETITIONS=10 \
-GB_AGGREGATES_ONLY=false \
-./scripts/run_april_suite.sh --only force native-novec
-
 # GB_REPETITIONS=10 \
 # GB_AGGREGATES_ONLY=false \
-# ./scripts/run_april_suite.sh --only hardcoded native
+# ./scripts/run_april_suite.sh --only force native-novec
 
-# ARGON_DT=0.005 \
-# ARGON_STRONG_REPEATS=3 \
-# ./scripts/run_april_suite.sh --only strong native
+GB_REPETITIONS=10 \
+GB_AGGREGATES_ONLY=false \
+./scripts/run_april_suite.sh --only hardcoded native
 
-# ARGON_DT=0.0000001 \
-# ARGON_STRONG_REPEATS=3 \
-# ./scripts/run_april_suite.sh --only strong native
+ARGON_DT=0.005 \
+ARGON_STRONG_REPEATS=3 \
+./scripts/run_april_suite.sh --only strong native
+
+ARGON_DT=0.0000001 \
+ARGON_STRONG_REPEATS=3 \
+./scripts/run_april_suite.sh --only strong native
 
 # -------------------------
 # LAMMPS
 # -------------------------
 
-# LAMMPS_FORCE_REPEATS=3 \
-# LAMMPS_ARGON_REPEATS=3 \
-# ./scripts/run_lammps_suite.sh --only both openmp-native
+LAMMPS_FORCE_REPEATS=3 \
+LAMMPS_ARGON_REPEATS=3 \
+./scripts/run_lammps_suite.sh --only both openmp-native
 
-# LAMMPS_FORCE_REPEATS=3 \
-# LAMMPS_ARGON_REPEATS=3 \
-# LAMMPS_MPI_RANKS_MODE=threads \
-# ./scripts/run_lammps_suite.sh --only both intel-native
+LAMMPS_FORCE_REPEATS=3 \
+LAMMPS_ARGON_REPEATS=3 \
+LAMMPS_MPI_RANKS_MODE=threads \
+./scripts/run_lammps_suite.sh --only both intel-native
 
 
 
